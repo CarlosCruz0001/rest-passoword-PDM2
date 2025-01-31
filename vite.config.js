d8a795799df1588e752b7a2687bce9a284a54282
CarlosCruz0001 <carloscrus0901@.com>
2025-01-30 22:15:42 -0300

    atualização de configuração vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Define o caminho base correto para o Vercel
   server: {
-    historyApiFallback: true
+    historyApiFallback: true // Garante que as rotas sejam tratadas corretamente
   }
-})
+});
